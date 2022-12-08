77ce22c466912ce51aef3ea58f7647ebe9e6bceb
afadillahazka <89429041+afadillahazka@users.noreply.github.com>
2022-12-09 06:47:31 +0700

    first commit

---
+++ src/api/fourthncafa/controllers/fourthncafa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fourthncafa controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fourthncafa.fourthncafa');
---
+++ src/api/fourthncafa/routes/fourthncafa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fourthncafa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::fourthncafa.fourthncafa');
