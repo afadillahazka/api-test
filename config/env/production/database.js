b11d88a8bc4224686fe0a2c140c7893b178ee471
afadillahazka <89429041+afadillahazka@users.noreply.github.com>
2022-12-09 07:44:21 +0700

    database and server

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,21 @@
-    // path: ./config/env/production/database.js
+// path: ./config/env/production/database.js
 
-    module.exports = ({ env }) => ({
-        connection: {
-          client: 'postgres',
-          connection: {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT'),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
-            ssl: {
-                ca: env('DATABASE_CA')
-            },
-          },
-          debug: false,
-        },
-      });
-  
-  
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
